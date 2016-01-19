6485f9f74a06e3a73d7ac5d3e01ec0ebd2d95cc4
Kyle Engelken <kyle.engelken@cerner.com>
2016-01-19 16:44:26 -0600

    Most of rigidbody done, still no movement.

--- Shoot 'Em Up/src/com/evilzoidberg/entities/Rigidbody.java
+++ Shoot 'Em Up/src/com/evilzoidberg/entities/Rigidbody.java
@@ -7,6 +7,10 @@ public class Rigidbody extends Entity {
 	public static float baseGravity = 5;
 	float xVel = 0; //Velocities never change for base rigidbody unless
 	float yVel = 0; //some outside object changes them, children could differ
+	boolean grounded = false;
+	float x2, y2; //Center of entity
+	float x3, y3; //Bottom right corner of entity
+	
 
 	public Rigidbody(Image image, float x, float y) {
 		super(image, x, y);
@@ -19,6 +23,58 @@ public class Rigidbody extends Entity {
 	 * @param objects - objects that the rigidbody should collide with
 	 */
 	public void update(int delta, Entity[] objects) {
+		yVel += baseGravity;
 		
+		//Check if entity should be falling
+		if(xVel == 0) {
+			for(int i = 0; i < yVel; i++) {
+				moveY(y > 0, objects); //move adjusts velocity, so it will end if hits ground
+			}
+		}
+		else {
+			float toMoveX = xVel;
+			float toMoveY = yVel;
+			
+			if(xVel > yVel) {
+				float ratio = xVel / yVel; //Proportion of velocities, so moved in about a straight line
+				
+				//Add movement
+			}
+			else {
+				float ratio = yVel / xVel; //Proportion of velocities, so moved in about a straight line
+				
+				//Add movement
+			}
+		}
+	}
+	
+	/**
+	 * Moves object left or right one unit and checks collisions
+	 * @param down - pass xVel > 0 and you get the correct value
+	 * @param objects - objects to collide with
+	 * @return
+	 */
+	public void moveX(boolean right, Entity[] objects) {
+		
+	}
+	
+	/**
+	 * Moves object up or down one unit and checks collisions
+	 * @param down - pass yVel > 0 and you get the correct value
+	 * @param objects - objects to collide with
+	 * @return
+	 */
+	public void moveY(boolean down, Entity[] objects) {
+		grounded = false;
+		y += 1;
+		for(Entity e : objects) {
+			if(e.intersects(this)) {
+				grounded = true;
+				yVel = 0;
+				y -= 1; //Undo test movement
+				
+				return; //No need to check more
+			}
+		}
 	}
 }
